43b542c750a711c745e509d36274c65fb690b3b2
Ingrid da Silva Oliveira <ingrid.oliveiratk@gmail.com>
2022-07-13 19:38:36 -0300

    criado toolbar, barra de pesquisa e menu lateral

--- solutions/devsprint-mychelle-rocha-1/app/src/main/java/com/devpass/githubapp/presentation/RepositoryListActivity.kt
+++ solutions/devsprint-mychelle-rocha-1/app/src/main/java/com/devpass/githubapp/presentation/RepositoryListActivity.kt
@@ -1,10 +1,16 @@
 package com.devpass.githubapp.presentation
 
+import android.app.SearchManager
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuInflater
+import android.widget.SearchView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.ui.AppBarConfiguration
 import android.widget.Toast
+import com.devpass.githubapp.R
 import com.devpass.githubapp.data.api.GitHubEndpoint
 import com.devpass.githubapp.data.model.Repository
 import com.devpass.githubapp.databinding.ActivityMainBinding
@@ -13,7 +19,7 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-class RepositoryListActivity : AppCompatActivity() {
+class RepositoryListActivity : AppCompatActivity(), SearchView.OnQueryTextListener {
 
     private lateinit var binding: ActivityMainBinding
 
@@ -24,21 +30,42 @@ class RepositoryListActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         setSupportActionBar(binding.toolbar)
+    }
 
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu_main, menu)
+        val searchView = menu.findItem(R.id.serch_toolbar).actionView as SearchView
+        searchView.setOnQueryTextListener(this)
+        return true
+    }
+
+    override fun onQueryTextSubmit(p0: String?): Boolean {
+        searchToolbar(p0)
+        return true
+    }
+
+    override fun onQueryTextChange(p0: String?): Boolean {
+        p0?.let { Log.i("TextChange", it) }
+        return true
+    }
+
+    private fun searchToolbar(p0: String?) {
         val retrofitClient = NetworkUtils.getRetrofitInstance("https://api.github.com")
         val endpoint = retrofitClient.create(GitHubEndpoint::class.java)
-        val callback = endpoint.getRepositories("devpass-tech")
+        val callback = p0?.let { endpoint.getRepositories(it) }
 
-        callback.enqueue(object : Callback<List<Repository>> {
+        callback?.enqueue(object : Callback<List<Repository>> {
             override fun onFailure(call: Call<List<Repository>>, t: Throwable) {
                 Toast.makeText(baseContext, t.message, Toast.LENGTH_SHORT).show()
             }
 
-            override fun onResponse(call: Call<List<Repository>>, response: Response<List<Repository>>) {
-
+            override fun onResponse(
+                call: Call<List<Repository>>,
+                response: Response<List<Repository>>
+            ) {
                 response.body()?.toString()?.let { Log.d("BODY", it) }
             }
         })
     }
-
 }
