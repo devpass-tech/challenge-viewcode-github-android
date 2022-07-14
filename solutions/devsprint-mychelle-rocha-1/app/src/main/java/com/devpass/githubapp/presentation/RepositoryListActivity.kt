17c603f004cc4e63fa4c1a0758565fdaec7c5306
Ingrid da Silva Oliveira <ingrid.oliveiratk@gmail.com>
2022-07-14 16:37:09 -0300

    resolução codeReview

--- solutions/devsprint-mychelle-rocha-1/app/src/main/java/com/devpass/githubapp/presentation/RepositoryListActivity.kt
+++ solutions/devsprint-mychelle-rocha-1/app/src/main/java/com/devpass/githubapp/presentation/RepositoryListActivity.kt
@@ -1,14 +1,11 @@
 package com.devpass.githubapp.presentation
 
-import android.app.SearchManager
-import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.Menu
 import android.view.MenuInflater
 import android.widget.SearchView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.ui.AppBarConfiguration
 import android.widget.Toast
 import com.devpass.githubapp.R
 import com.devpass.githubapp.data.api.GitHubEndpoint
@@ -18,6 +15,8 @@ import com.devpass.githubapp.utils.NetworkUtils
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+
+private const val SEARCH_REPOSITORY = "devpass-tech"
 
 class RepositoryListActivity : AppCompatActivity(), SearchView.OnQueryTextListener {
 
@@ -30,6 +29,7 @@ class RepositoryListActivity : AppCompatActivity(), SearchView.OnQueryTextListen
         setContentView(binding.root)
 
         setSupportActionBar(binding.toolbar)
+        getListRepository(SEARCH_REPOSITORY)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -40,20 +40,19 @@ class RepositoryListActivity : AppCompatActivity(), SearchView.OnQueryTextListen
         return true
     }
 
-    override fun onQueryTextSubmit(p0: String?): Boolean {
-        searchToolbar(p0)
+    override fun onQueryTextSubmit(searchRepository: String?): Boolean {
+        getListRepository(searchRepository)
         return true
     }
 
-    override fun onQueryTextChange(p0: String?): Boolean {
-        p0?.let { Log.i("TextChange", it) }
+    override fun onQueryTextChange(searchRepository: String?): Boolean {
         return true
     }
 
-    private fun searchToolbar(p0: String?) {
+    private fun getListRepository(searchToolbar: String?) {
         val retrofitClient = NetworkUtils.getRetrofitInstance("https://api.github.com")
         val endpoint = retrofitClient.create(GitHubEndpoint::class.java)
-        val callback = p0?.let { endpoint.getRepositories(it) }
+        val callback = searchToolbar?.let { endpoint.getRepositories(it) }
 
         callback?.enqueue(object : Callback<List<Repository>> {
             override fun onFailure(call: Call<List<Repository>>, t: Throwable) {
