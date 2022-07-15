5c1c15acfdc343a18d2204ad57cbabeee73faafb
adenilsonricardo <85114978+adenilsonricardo@users.noreply.github.com>
2022-07-15 16:43:15 -0300

    create repository_owner_fragment and RepositoryOwnerFragment kt

--- solutions/devsprint-mychelle-rocha-1/app/src/main/java/com/devpass/githubapp/presentation/RepositoryOwnerFragment.kt
+++ solutions/devsprint-mychelle-rocha-1/app/src/main/java/com/devpass/githubapp/presentation/RepositoryOwnerFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
 import com.devpass.githubapp.data.model.Owner
 import com.devpass.githubapp.databinding.RepositoryOwnerFragmentBinding
 
@@ -30,6 +31,10 @@ class RepositoryOwnerFragment : Fragment() {
     }
 
     private fun repositoryOwner(owner: Owner) {
+        binding.tvOwnerName.text = owner.login
         binding.tvOwnerBio.text = owner.url
+        Glide.with(binding.root)
+            .load(owner.avatarUrl)
+            .into(binding.ivOwnerAvatar)
     }
 }
