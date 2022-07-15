690bd3afd8ef0fc9bbc001900a80e5884d40c2fa
adenilsonricardo <85114978+adenilsonricardo@users.noreply.github.com>
2022-07-15 16:14:21 -0300

    created repository_license_fragment and RepositoryLicenseFragment kt

---
+++ solutions/devsprint-mychelle-rocha-1/app/src/main/java/com/devpass/githubapp/presentation/RepositoryLicenseFragment.kt
@@ -0,0 +1,39 @@
+package com.devpass.githubapp.presentation
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.devpass.githubapp.data.model.License
+import com.devpass.githubapp.databinding.RepositoryLicenseFragmentBinding
+
+class RepositoryLicenseFragment : Fragment() {
+
+    private var _binding: RepositoryLicenseFragmentBinding? = null
+
+    private val binding get() = _binding!!
+    private lateinit var license: License
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = RepositoryLicenseFragmentBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    private fun repositoryLicense(license: License) {
+        binding.tvLicenseName.text = license.name
+        binding.tvLicenseCod.text = license.key
+        binding.tvLicenseViewMore.setOnClickListener {
+            val intent = Intent(
+                context,
+                "Colocar a activity que o botão está direcionando"::class.java
+            )
+            startActivity(intent)
+        }
+    }
+}
