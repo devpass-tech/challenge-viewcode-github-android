2dcfa26b6131043c28a342501f5f1a0f03506dc2
adenilsonricardo <85114978+adenilsonricardo@users.noreply.github.com>
2022-07-15 14:47:41 -0300

    create repository_owner_fragment and RepositoryOwnerFragment.kt

---
+++ solutions/devsprint-mychelle-rocha-1/app/src/main/java/com/devpass/githubapp/presentation/RepositoryOwnerFragment.kt
@@ -0,0 +1,35 @@
+package com.devpass.githubapp.presentation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.devpass.githubapp.data.model.Owner
+import com.devpass.githubapp.databinding.RepositoryOwnerFragmentBinding
+
+class RepositoryOwnerFragment: Fragment() {
+
+    private var _binding: RepositoryOwnerFragmentBinding? = null
+
+    private val binding get() = _binding!!
+    private lateinit var owner: Owner
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding  = RepositoryOwnerFragmentBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        repositoryOwner(owner)
+    }
+
+    private fun repositoryOwner(owner: Owner){
+        binding.tvBioOwner.text = owner.url
+    }
+}
