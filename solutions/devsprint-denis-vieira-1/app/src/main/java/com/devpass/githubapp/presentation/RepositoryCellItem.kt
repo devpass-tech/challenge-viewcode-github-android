4a1107a56fd5a4a370539c6a7b5fd7cb814658cf
vitoriaseabra <vitoriaseabra_@hotmail.com>
2022-08-25 10:47:23 -0300

    Implement RecyclerView and adapter to RepositoryCellItem

--- solutions/devsprint-denis-vieira-1/app/src/main/java/com/devpass/githubapp/presentation/RepositoryCellItem.kt
+++ solutions/devsprint-denis-vieira-1/app/src/main/java/com/devpass/githubapp/presentation/RepositoryCellItem.kt
@@ -1,13 +1,26 @@
 package com.devpass.githubapp.presentation
 
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.devpass.githubapp.R
+import com.devpass.githubapp.data.model.Repository
 import com.devpass.githubapp.databinding.ItemCellRepositoryBinding
 
 class RepositoryCellItem(
-    binding: ItemCellRepositoryBinding
+    private val binding: ItemCellRepositoryBinding
 ) : RecyclerView.ViewHolder(binding.root) {
 
-    fun bind() {
+    fun bind(item: Repository) {
+        with(binding){
+            textviewRepositoryName.text = item.name
+            textviewOwnerName.text = item.owner.login
 
+            Glide
+                .with(root.context)
+                .load(item.owner.avatarUrl)
+                .centerCrop()
+                .placeholder(R.drawable.ic_placeholder)
+                .into(imageviewAvatar)
+        }
     }
 }
