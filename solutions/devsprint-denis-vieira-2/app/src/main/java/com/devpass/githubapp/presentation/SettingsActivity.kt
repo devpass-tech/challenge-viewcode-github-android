ea0313bbd96498fc70f5d9e09a53922296bbf535
Indira Singh <lucindhy@gmail.com>
2022-09-15 19:38:01 -0300

    Interface Settings

---
+++ solutions/devsprint-denis-vieira-2/app/src/main/java/com/devpass/githubapp/presentation/SettingsActivity.kt
@@ -0,0 +1,27 @@
+package com.devpass.githubapp.presentation
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.devpass.githubapp.R
+import com.devpass.githubapp.databinding.SettingsActivityBinding
+
+class SettingsActivity : AppCompatActivity() {
+
+    private lateinit var binding: SettingsActivityBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = SettingsActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbarSettings)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+    }
+}
