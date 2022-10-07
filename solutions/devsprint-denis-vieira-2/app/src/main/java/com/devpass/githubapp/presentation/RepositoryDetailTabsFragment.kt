bcbcc7b6f58b5fead991e56023d6fa5078d89a31
Renato Marciano <37446964+RenatoMarciano@users.noreply.github.com>
2022-10-07 17:46:24 -0300

    Repository detail tabs fragment (#36)
    * Implementação de interface para mostra 3 abas
    
    * Implementando boa praticas nas interfaces
    
    * Tirando os texto soltos no codigo e colocando em uma String resource

--- solutions/devsprint-denis-vieira-2/app/src/main/java/com/devpass/githubapp/presentation/RepositoryDetailTabsFragment.kt
+++ solutions/devsprint-denis-vieira-2/app/src/main/java/com/devpass/githubapp/presentation/RepositoryDetailTabsFragment.kt
@@ -1,19 +1,54 @@
 package com.devpass.githubapp.presentation
 
+
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.graphics.Insets.add
+import androidx.core.view.OneShotPreDrawListener.add
 import com.devpass.githubapp.R
 
+import com.devpass.githubapp.adapter.TabAdapter
+import com.devpass.githubapp.databinding.FragmentRepositoryDetailTabsBinding
+import com.google.android.material.tabs.TabLayoutMediator
+
 class RepositoryDetailTabsFragment : Fragment() {
+
+    private lateinit var binding: FragmentRepositoryDetailTabsBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_repository_detail_tabs, container, false)
+
+        binding = FragmentRepositoryDetailTabsBinding.inflate(inflater, container, false)
+        return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupTabs()
+    }
+
+    private fun setupTabs() {
+        binding.viewPagerTabs.adapter = TabAdapter(this)
+        val about: String = getString(R.string.tab_about)
+        val owner: String = getString(R.string.tab_owner)
+        val license: String = getString(R.string.tab_license)
+
+        TabLayoutMediator(binding.tabsButtonsDetail, binding.viewPagerTabs) { tab, position ->
+            tab.text =
+                when(position) {
+                    0 -> about
+                    1 -> owner
+                    2 -> license
+                    else -> ""
+                }
+        }.attach()
+    }
+
 }
--- solutions/devsprint-denis-vieira-2/app/src/main/java/com/devpass/githubapp/presentation/RepositoryDetailsActivity.kt
+++ solutions/devsprint-denis-vieira-2/app/src/main/java/com/devpass/githubapp/presentation/RepositoryDetailsActivity.kt
@@ -34,5 +34,8 @@ class RepositoryDetailsActivity : AppCompatActivity() {
 
         }
 
+        supportFragmentManager.beginTransaction().add(R.id.container_header, RepositoryDetailsHeaderFragment()).commit()
+
+        supportFragmentManager.beginTransaction().add(R.id.container_tabs, RepositoryDetailTabsFragment()).commit()
     }
 }
